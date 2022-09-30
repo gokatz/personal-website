d19b91617f3eef9daacf0b5e052963352446aa5e
Gokul Kathirvel <gokulkathirvel@live.com>
2022-09-30 12:35:34 +0000

    update GA4

--- src/data/index.js
+++ src/data/index.js
@@ -6,5 +6,5 @@ export default {
     Passionate about Web performance and Ember. Gokul Kathirvel writes blogs 
     on Ember, Reatc, Chrome extension, Performance etc.`,
   permalink: 'https://gokatz.me',
-  gaId: 'UA-104177053-2'
+  gaId: 'G-C1G0YHKD5X' // G4 Analytics
 }
