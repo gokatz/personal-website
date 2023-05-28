e0a550c3d81c7a8e9737349ea8f4724f8a050a8b
Gokul Kathirvel <gokulkathirvel@live.com>
2023-05-28 09:49:03 +0530

    Update BuyMeACoffee.js
    update buy me coffee link

--- src/components/Sidebar/BuyMeACoffee/BuyMeACoffee.js
+++ src/components/Sidebar/BuyMeACoffee/BuyMeACoffee.js
@@ -3,7 +3,7 @@ import styles from './BuyMeACoffee.module.scss';
 
 const BuyMeACoffee = () => (
   <div style={{ marginBottom: '2em' }}>
-    <a href="https://www.buymeacoffee.com/Z893eGMRB" target="_blank">
+    <a href="https://www.buymeacoffee.com/gokatz" target="_blank">
       <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" className={styles['buy-me-cta']} />
     </a>
   </div>
